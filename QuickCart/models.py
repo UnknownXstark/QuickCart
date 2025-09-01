50fef20d8c57e146ebfe3ccf1e119710e3c437c1
UnknownXstark <nathanlouiskalio@gmail.com>
2025-09-01 23:02:58 +0100

    product class

--- QuickCart/models.py
+++ QuickCart/models.py
@@ -12,3 +12,9 @@ class User:
         self.password = password
         self.role = role
         self.orders = []
+
+class Product:
+    def __init__(self, name, price, stock=0):
+        self.name = name
+        self.price = price
+        self.stock = stock
