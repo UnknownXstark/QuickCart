0b840f940a80e86603363d185c7154f8999cf486
UnknownXstark <nathanlouiskalio@gmail.com>
2025-09-02 00:02:54 +0100

    updates to services

--- QuickCart/service.py
+++ QuickCart/service.py
@@ -3,3 +3,9 @@ from models import User, Rider, Product, Order, Role
 users = []
 products = []
 orders = []
+
+def register(username, password, role=Role.USER):
+    if any(u.username == username for u in users):
+        return None
+    if role == Role.RIDER:
+        
