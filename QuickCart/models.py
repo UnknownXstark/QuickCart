7a1795616589ef675e303f825ddcd1da41847a99
UnknownXstark <nathanlouiskalio@gmail.com>
2025-09-01 22:57:48 +0100

    user class

--- QuickCart/models.py
+++ QuickCart/models.py
@@ -5,3 +5,10 @@ class Role(Enum):
     ADMIN = "Admin"
     USER = "User"
     RIDER = "Rider"
+
+class User:
+    def __init__(self, username, password, role=Role.USER):
+        self.username = username
+        self.password = password
+        self.role = role
+        self.orders = []
