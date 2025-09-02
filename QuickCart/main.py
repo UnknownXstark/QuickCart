9f5d9de477de347f7ca50bacc337194f0e611bfb
UnknownXstark <nathanlouiskalio@gmail.com>
2025-09-02 13:06:53 +0100

    updates

--- QuickCart/main.py
+++ QuickCart/main.py
@@ -1,3 +1,12 @@
 from service import register, login
 from menus import admin_menu, user_menu, riders_menu
 from models import Role
+
+register("admin", "admin", Role.ADMIN)
+
+while True:
+    print("\n--- QuickCart ---")
+    print("1. Register")
+    print("2. Login")
+    print("3. Exit")
+    choice = input("Choose: ")
