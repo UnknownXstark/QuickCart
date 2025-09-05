4eb603c67d9ed393a148bca475d235d22ebd6f5a
UnknownXstark <nathanlouiskalio@gmail.com>
2025-09-05 12:27:02 +0100

    storage functions

--- QuickCart/storage.py
+++ QuickCart/storage.py
@@ -2,4 +2,11 @@ import json
 import os
 
 def save_data(data, filename):
-    
+    with open(filename, "w") as f:
+        json.dump(data, f, indent=4)
+
+def load_data(filename):
+    if not os.path.exists(filename):
+        return[]
+    with open(filename, "r") as f:
+        return json.load(f)
